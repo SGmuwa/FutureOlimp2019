d8f3ad5af26a1ed06b703a0b5a31340d7e169750
SG_Muwa <motherlode.muwa@gmail.com>
2019-11-09 16:09:26 +0300

    Сделал задание 10.

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Text.RegularExpressions;
+using System.Numerics;
 
 
 namespace Task10
@@ -12,9 +12,44 @@ namespace Task10
         public static void Main(string[] args)
         {
             Console.WriteLine(About);
-            string str = Console.ReadLine();
-            Console.WriteLine(
-                Regex.IsMatch(str, "(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{12,}") ? "YES" : "NO");
+            Console.WriteLine(ReaderAdder());
+        }
+
+        public static BigInteger ReaderAdder()
+        {
+            BigInteger number = GetNumber("Введите число: ", new TryParseHandler<BigInteger>(BigInteger.TryParse));
+            if(number == 0)
+                return 0;
+            if(number % 8 != 0)
+                number = 0;
+            return number + ReaderAdder();
+        }
+
+        /// <summary>
+        /// Класс-делекат, который описывает интерфейс функций TryParse.
+        /// </summary>
+        /// <param name="inputStr">Входная строка, которую надо преобразовать в объект.</param>
+        /// <param name="result">Указатель на результат выполнения.</param>
+        /// <typeparam name="T">Требуемый тип искомого объекта.</typeparam>
+        /// <returns>True, если чтение удачно. Иначе - false.</returns>
+		public delegate bool TryParseHandler<T>(string inputStr, out T result);
+
+        /// <summary>
+        /// Считывает с пользовательского интерфейса объект.
+        /// </summary>
+        /// <param name="message">Сообщение, которое надо напечатать пользователю.</param>
+        /// <param name="parse">Метод считывания TryParse, с помощью которого будет преобразована строка в объект.</param>
+        /// <typeparam name="T">Указывает, в какой тип надо преобразовывать объект.</typeparam>
+        /// <returns>Считанный объект от пользователя.</returns>
+        public static T GetNumber<T>(string message, TryParseHandler<T> parse)
+        {
+            while(true)
+            {
+                Console.Write(message);
+                if(parse(Console.ReadLine(), out T output))
+                    return output;
+                Console.WriteLine($"\"{output}\" не может быть прочитан с помощью {parse.Method}");
+            }
         }
     }
 }
